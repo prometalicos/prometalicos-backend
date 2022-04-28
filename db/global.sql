562ecb7ad30e9625816b0e3f38dab8718fbb43e1
Andres Mauricio Martinez Hincapie <amartinezh@gmail.com>
2022-04-28 12:43:55 -0500

    ajuste menu

--- db/global.sql
+++ db/global.sql
@@ -60,41 +60,11 @@ CREATE TABLE adm.usuario_sub_sistema(
 
 CREATE TABLE adm.usuario(
 	usuario_id varchar(64) not null,
-        tipo_usuario_id varchar(32) not null,
+        rol_id int not null,
         nombre_completo varchar(256) not null,
         direccion varchar(256) not null,
         estado bit not null,
         CONSTRAINT usuario_pk PRIMARY KEY (usuario_id)
-        )
-          WITH (
-             OIDS=FALSE
-        );
-
-CREATE TABLE adm.tipo_usuario(
-	tipo_usuario_id varchar(32) not null,
-        descripcion varchar(256) not null,
-        CONSTRAINT tipo_usuario_pk PRIMARY KEY (tipo_usuario_id)
-        )
-          WITH (
-             OIDS=FALSE
-        );
-
-CREATE TABLE adm.usuario_menu(
-	usuario_id varchar(64) not null,
-        opciones_menu_id int not null, -- Solo se debe permitir asignar opciones_menu sin padre
-        restricciones varchar(2), -- Primero se indica R (Only read) y W (Only Write) y - vacio, ejemplo: -W
-        CONSTRAINT usuario_menu_pk PRIMARY KEY (usuario_id, opciones_menu_id)
-        )
-          WITH (
-             OIDS=FALSE
-        );
-
-CREATE TABLE adm.opciones_menu(
-	opciones_menu_id int not null,
-        opciones_menu_id_padre int not null,
-        descripcion varchar(64) not null,
-        url varchar(256) not null,
-        CONSTRAINT opciones_menu_pk PRIMARY KEY (opciones_menu_id)
         )
           WITH (
              OIDS=FALSE
@@ -167,10 +137,6 @@ END; $$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER user_log AFTER INSERT ON adm.log FOR EACH ROW EXECUTE PROCEDURE log_user();
 
-ALTER TABLE adm.usuario ADD CONSTRAINT FK_usuario__tipo_usuario FOREIGN KEY (tipo_usuario_id) REFERENCES adm.tipo_usuario (tipo_usuario_id);
-ALTER TABLE adm.usuario_menu ADD CONSTRAINT FK_usuario_menu__usuario FOREIGN KEY (usuario_id) REFERENCES adm.usuario (usuario_id);
-ALTER TABLE adm.usuario_menu ADD CONSTRAINT FK_usuario_menu__opciones_menu FOREIGN KEY (opciones_menu_id) REFERENCES adm.opciones_menu (opciones_menu_id);
-
 ALTER TABLE adm.sede ADD CONSTRAINT FK_sede__concesion FOREIGN KEY (concesion_id) REFERENCES adm.concesion (concesion_id);
 ALTER TABLE adm.sub_sistema ADD CONSTRAINT FK_sede__sub_sistema FOREIGN KEY (sede_id) REFERENCES adm.sede (sede_id);
 ALTER TABLE adm.usuario_sub_sistema ADD CONSTRAINT FK_usuario_sub_sistema__usuario FOREIGN KEY (usuario_id) REFERENCES adm.usuario (usuario_id);
@@ -182,4 +148,78 @@ ALTER TABLE adm.periferico ADD CONSTRAINT FK_periferico__tipo_periferico FOREIGN
 
 ALTER TABLE adm.eventos_usuario ADD CONSTRAINT FK_eventos_usuario__usuario FOREIGN KEY (usuario_id) REFERENCES adm.usuario (usuario_id);
 
+CREATE TABLE adm.permiso (
+    permiso_id int NOT NULL,
+    father int NOT NULL,
+    level int NOT NULL,
+    name character varying(64),
+    icon character varying(64),
+    url character varying(128),
+    CONSTRAINT permiso_pk PRIMARY KEY (permiso_id)
+);
+
+CREATE TABLE adm.rol (
+    rol_id int NOT NULL,
+    name character varying(64),
+    CONSTRAINT rol_pk PRIMARY KEY (rol_id)
+);
+
+CREATE TABLE adm.usuario_rol (
+    usuario_id varchar(64) not null,
+    rol_id int NOT NULL,
+    CONSTRAINT usuario_rol_pk PRIMARY KEY (usuario_id, rol_id)
+);
+
+ALTER TABLE adm.usuario ADD CONSTRAINT FK_usuario__rol FOREIGN KEY (rol_id) REFERENCES adm.rol (rol_id);
+ALTER TABLE adm.usuario_rol ADD CONSTRAINT FK_usuario_rol__usuario FOREIGN KEY (usuario_id) REFERENCES adm.usuario (usuario_id);
+ALTER TABLE adm.usuario_rol ADD CONSTRAINT FK_usuario_rol__rol FOREIGN KEY (rol_id) REFERENCES adm.rol (rol_id);
+
+CREATE TABLE adm.permiso_rol (
+    permiso_id int NOT NULL,
+    rol_id int NOT NULL,
+    CONSTRAINT permiso_rol_pk PRIMARY KEY (permiso_id, rol_id)
+);
+
+ALTER TABLE adm.permiso_rol ADD CONSTRAINT FK_permiso_rol__permiso FOREIGN KEY (permiso_id) REFERENCES adm.permiso (permiso_id);
+ALTER TABLE adm.permiso_rol ADD CONSTRAINT FK_permiso_rol FOREIGN KEY (rol_id) REFERENCES adm.rol (rol_id);
+
+INSERT INTO adm.rol(rol_id, name) VALUES(00, 'SuperUser');
+INSERT INTO adm.rol(rol_id, name) VALUES(11, 'Administrador POS');
+INSERT INTO adm.rol(rol_id, name) VALUES(12, 'Empleado POS');
+INSERT INTO adm.rol(rol_id, name) VALUES(13, 'Invitado POS');
+INSERT INTO adm.rol(rol_id, name) VALUES(21, 'Administrador Parking');
+INSERT INTO adm.rol(rol_id, name) VALUES(22, 'Empleado Parking');
+INSERT INTO adm.rol(rol_id, name) VALUES(23, 'Invitado Parking');
+INSERT INTO adm.rol(rol_id, name) VALUES(31, 'Contador');
+INSERT INTO adm.rol(rol_id, name) VALUES(34, 'Auxiliar Contable');
+
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(1, -1, 0, 'Principal', 'icon-basket-loaded', '-');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(2, -1, 0, 'Administracion', 'icon-settings', '-');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(3, -1, 0, 'Parqueadero', 'icon-flag', '-');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(4, -1, 0, 'Contabilidad', 'icon-loop', '-');
+
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(11, 1, 1, 'Ventas', 'icon-basket', '/sales');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(12, 1, 1, 'Pagos de clientes', 'icon-user-follow', '/customer/customer-payments');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(13, 1, 1, 'Movimientos de caja', 'icon-handbag', '/close-cash/cash-movement');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(14, 1, 1, 'Cierre de caja', 'icon-calculator', '/close-cash');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(15, 1, 1, 'Facturas', 'icon-book-open', '/close-cash/receipts');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(16, 1, 1, 'Ordenes', 'icon-basket', '/orders');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(17, 1, 1, 'Ordenes Listas', 'icon-basket', '/orders/list-ready');
+
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(21, 2, 1, 'Proveedores', 'icon-user', '/supplier');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(22, 2, 1, 'Clientes', 'icon-user', '/customer');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(23, 2, 1, 'Inventario', 'icon-home', '/stock');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(24, 2, 1, 'Reportes de Ventas', 'icon-basket', '/sales/menu');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(25, 2, 1, 'Mantenimiento', 'icon-wrench', '/maintenance');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(26, 2, 1, 'Configuración', 'icon-settings', '/configuration');
+
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(31, 3, 1, 'Ingreso', 'icon-arrow-right', '/parking/places/p');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(32, 3, 1, 'Vehiculos', 'icon-rocket ', '/parking/vehicle-panel');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(33, 3, 1, 'Reportes', 'icon-book-open', '/parking/rep');
+INSERT INTO adm.permiso (permiso_id, father, level, name, icon, url) VALUES(34, 3, 1, 'Configuración', 'icon-settings', '/parking/configuration');
+
+INSERT INTO adm.permiso_rol (permiso_id, rol_id) VALUES(1, 00);
+INSERT INTO adm.permiso_rol (permiso_id, rol_id) VALUES(2, 00);
+INSERT INTO adm.permiso_rol (permiso_id, rol_id) VALUES(3, 00);
+INSERT INTO adm.permiso_rol (permiso_id, rol_id) VALUES(4, 00);
 

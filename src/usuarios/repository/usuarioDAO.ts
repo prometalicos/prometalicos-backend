7232d90ef485aa59f38a789e5fa392f41d22a588
ceul <ceul@ceul.ceul>
2022-04-29 15:49:03 -0500

    Se comienza a desarrollar la funcion que retorna el menu construido hacia el frontend

--- src/usuarios/repository/usuarioDAO.ts
+++ src/usuarios/repository/usuarioDAO.ts
@@ -143,6 +143,49 @@ export class UsuariosDAO {
         }
     }
 
+    public async getMenu(usuarioId: string, op?: any) {
+        let result = new Array();
+        try {
+            let con = await this.connection.getConnection();
+            let query_usuario_menu = await con.query(`SELECT
+                        usuario_id,
+                        opciones_menu_id
+                        FROM usario_menu
+                        WHERE usuario_id = $1;`, [usuarioId]);
+            if (query_usuario_menu.length > 0) {
+                let row: NavData;
+                row = <NavData>{ name: "Escritorio", icon: "icon-speedometer", url: "/dashboard" };
+                result.push(row);
+                for (let index_usario_menu = 0; index_usario_menu < query_usuario_menu.length; index_usario_menu++) {
+                    let query_permissions_roles = await con.query(`SELECT
+                            id_permission,
+                            opciones_menu_id
+                            FROM permissions_roles
+                            WHERE opciones_menu_id = ?;`, [query_usuario_menu[index_usario_menu]['opciones_menu_id']]);
+                    if (query_permissions_roles.length > 0) {
+                        for (let index_permissions_roles = 0; index_permissions_roles < query_permissions_roles.length; index_permissions_roles++) {
+                            let query_permissions = await con.query(`SELECT
+                                opciones_menu_id,
+                                descripcion,
+                                url
+                                FROM opciones_menu
+                                WHERE opciones_menu_id = $1 and opciones_menu_id_padre = -1;`, [query_permissions_roles[index_permissions_roles]['opciones_menu_id']]);
+                            if (query_permissions.length > 0) {
+                                for (let index_permissions = 0; index_permissions < query_permissions.length; index_permissions++) {
+                                    //result = await this.getPermissions(query_permissions[index_permissions], result, op);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            con.release();
+            return result;
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
     public async getSubMenu(permissionId: string) {
         try {
 

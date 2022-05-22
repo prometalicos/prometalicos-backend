443d82699d509abb66273aa6401fac712b3e6f1a
ceul <ceul@ceul.ceul>
2022-05-22 11:33:51 -0500

    se sigue desarrollando el watcher

--- src/ftpWatcher.ts
+++ src/ftpWatcher.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as ch from "chokidar";
 
 
@@ -7,8 +8,31 @@ export class FtpWatcher {
     private static instance: FtpWatcher;
 
     private constructor() {
-        ch.watch('/home/ceul/Descargas/ftp').on('all', (event, path) => {
-            console.log(event, path);
+        let watchOptions = {
+            persistent: true,
+            ignoreInitial: true,
+            awaitWriteFinish: true,
+            //usePolling: false,
+            ignorePermissionErrors: false
+        };
+        ch.watch('/home/ceul/Descargas/ftp', watchOptions).on('add', (event) => {
+
+            let data = fs.readFileSync(event,
+                { encoding: 'utf8', flag: 'r' });
+
+            let test = data.split('/n')
+            data = null
+            let re = /FwV?(.*)/g
+            let x = re.exec(test[test.length - 1])
+            test = null
+            let result = x[0].split(';')
+            let properties: any = {}
+            for (let i = 0; i < result.length - 1; i++) {
+                let b = result[i].split('=')
+                properties[b[0]] = b[1]
+            }
+            console.log(properties)
+
         });
 
     }

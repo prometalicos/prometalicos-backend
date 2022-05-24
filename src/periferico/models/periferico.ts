7baa14c49e56144057805a48cf58c197cdd08cd4
ceul <ceul@ceul.ceul>
2022-05-23 21:49:33 -0500

    se desarrolla CRUD de periferico

---
+++ src/periferico/models/periferico.ts
@@ -0,0 +1,14 @@
+export class Periferico{
+    public periferico_id: string;
+    public sub_sistema_id: string;
+    public tipo_periferico_id: string;
+    public descripcion: string;
+    public marca: string;
+    public serial: string;
+    public modelo: string;
+    public voltaje: string;
+    public numero_puertos: string;
+    public ip: string;
+    public documento_identificacion: string;
+    public ruta_ftp: string;
+}

7c221c3c7d60cf59c8d70b05cd232921f5944bfa
Andres Mauricio Martinez Hincapie <amartinezh@gmail.com>
2022-07-21 09:35:51 -0500

    Ajuste en el orquestador

--- src/dimensionamiento/lectura_sensor_laser/repository/lectura_sensores_laserDAO.ts
+++ src/dimensionamiento/lectura_sensor_laser/repository/lectura_sensores_laserDAO.ts
@@ -30,7 +30,7 @@ export class LecturaSensoresLaserDAO {
                 direction,
                 wrong_way,
                 stop_and,
-                url_file_pds) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19);`, [
+                url_file_pds) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19) RETURNING lectura_sensores_id;`, [
                     periferico_id,
                     lecturaSensoresLaser.id, 
                     lecturaSensoresLaser.lane, 
